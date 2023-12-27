e57282829a7ffa20f09809ac8ab98bbe4fa8bcc6
Hisham-7007 <HishamCour>
2023-12-27 15:43:00 +0200

    first commit

---
+++ router/booksdb.js
@@ -0,0 +1,20 @@
+let books = {
+      1: {
+            "author": "Hisham Ibrahim", "title": "One Hundred Years of Solitude", "reviews": {
+                  'user1': 'hisham',
+                  'user2': 'max', 
+                  'user3': 'ibrahim'
+            }
+      },
+      2: { "author": "Max Hisham Andersen", "title": "Fairy tales", "reviews": {} },
+      3: { "author": "Dante Hisham", "title": "The Divine Comedy", "reviews": {} },
+      4: { "author": "Unknown", "title": "Moby Dick", "reviews": {} },
+      5: { "author": "Unknown", "title": "The Book Of Job", "reviews": {} },
+      6: { "author": "Unknown", "title": "One Thousand and One Nights", "reviews": {} },
+      7: { "author": "Unknown", "title": "The Brothers Karamazov", "reviews": {} },
+      8: { "author": "Jane Austen", "title": "Pride and Prejudice", "reviews": {} },
+      9: { "author": "Honor\u00e9 de Balzac", "title": "Ulysses", "reviews": {} },
+      10: { "author": "Samuel Beckett", "title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+}
+
+module.exports = books;
